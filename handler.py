02058e4bf9438c503eb63cfb0aecd17eab654765
macbook_2 <qa7626@gmail.com>
2026-07-24 17:56:23 +0900

    Fix: handler.py data = request.get("input", {}) 코드 추가

--- handler.py
+++ handler.py
@@ -9,10 +9,11 @@ from config import settings
 import torch
 from infra import r2_client, redis_client
 
-def process_result(data):
+def process_result(request):
     temp_paths = None
     face_detection = None
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    data = request.get("input", {})
     video_key = data.get("video_key")
     target_image_keys = data.get("target_image_keys")
     spot_list = data.get("spot_list")
