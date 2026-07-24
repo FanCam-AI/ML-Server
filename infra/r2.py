da5d5a02204d8b2976641e57d287dfcc35f072f0
macbook_2 <qa7626@gmail.com>
2026-07-24 17:16:43 +0900

    Fix: infra/r2.py 수정

--- infra/r2.py
+++ infra/r2.py
@@ -4,7 +4,7 @@ from config import settings
 def get_r2_client():
     r2 = boto3.client(
         "s3",
-        endpoint_url=settings.R2_ENDPOINT,
+        endpoint_url=settings.R2_ENDPOINT_URL,
         aws_access_key_id=settings.R2_ACCESS_KEY,
         aws_secret_access_key=settings.R2_SECRET_KEY,
         region_name="auto",
